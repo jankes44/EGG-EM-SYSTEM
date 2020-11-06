54ba9a8172434a332b2f64adbe48446cf264d869
cece95 <cesaregri@gmail.com>
2020-11-06 11:19:51 +0000

    added base for java server

---
+++ site/java server/src/test/java/com/egg/em_server/EmServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.egg.em_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
