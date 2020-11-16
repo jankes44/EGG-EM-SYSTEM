a0c3980c40fa39a8ee2ba8386663abf3f07683c8
cece95 <cesaregri@gmail.com>
2020-11-16 11:25:39 +0000

    updated dashboard

--- site/server/routes/api/powerData.js
+++ site/server/routes/api/powerData.js
@@ -5,178 +5,134 @@ const jwt = require("jsonwebtoken");
 const con = require("../../database/db2");
 const url = require('url');
 
+const fillTemplate = function(templateString, templateVars){
+  var func = new Function(...Object.keys(templateVars),  "return `"+templateString +"`;")
+  return func(...Object.values(templateVars));
+}
+
+const insertQuery = "INSERT INTO power_data (line, epoch, power, current ,voltage) VALUES ?"
+const getLinesQuery = "select distinct line from power_data order by line"
+const aggregatePowerQuery = "select ${line_} round(sum(kwh),2) as power_consumption from "  +
+"( select 	DATE_FORMAT((FROM_UNIXTIME(epoch)), '%Y-%m-%d-%H') as date_hour, " +
+"FROM_UNIXTIME(epoch) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
+"where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL ${timePeriod})) \
+group by date_hour, line ) t ${line}"
+
+const timePeriods = {
+  "day": "-1 DAY",
+  "month": "-1 MONTH",
+  "three_months": "-3 MONTH",
+  "year": "-1 YEAR"
+}
+
+const rangeQueries = {
+  "day": "select UNIX_TIMESTAMP(date_hour) as epoch, ${values} \
+          as 'values' from (select DATE_FORMAT((FROM_UNIXTIME(epoch)), '%Y-%m-%d-%H') as date_hour, line, \
+          avg(if (${measure} > 0, ${measure}, 0)) as ${measure} \
+          from power_data where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 DAY)) \
+          group by date_hour, line ) t group by epoch", 
+  
+  "week": "select UNIX_TIMESTAMP(CONCAT( date, '-', hour*6)) as epoch, ${values} as 'values' \
+            from ( \
+            select  DATE_FORMAT((FROM_UNIXTIME(epoch)), '%Y-%m-%d') as date, \
+                DATE_FORMAT((FROM_UNIXTIME(epoch)), '%H') DIV 6 as hour, line, \
+                avg(if (${measure} > 0, ${measure}, 0)) as ${measure} \
+            from power_data where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 WEEK)) \
+            group by date, line, hour) t group by epoch",
+  
+  "month": "select UNIX_TIMESTAMP(date_hour) as epoch, ${values} \
+            as 'values' from (select DATE_FORMAT((FROM_UNIXTIME(epoch)), '%Y-%m-%d') as date_hour, line, avg(if (${measure} > 0, ${measure} , 0)) as ${measure} \
+            from power_data where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 MONTH)) group by date_hour, line ) t group by epoch", 
+
+  "three_months":  "select UNIX_TIMESTAMP(date_hour) as epoch, ${values} \
+                    as 'values' from (select DATE_FORMAT((FROM_UNIXTIME(epoch)), '%Y-%m-%d') as date_hour, line, \
+                    avg(if (${measure} > 0, ${measure} , 0)) as ${measure} \
+                    from power_data where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -3 MONTH)) \
+                    group by date_hour, line ) t group by epoch",
+  
+   "year": "select UNIX_TIMESTAMP(week_start) as epoch, ${values} as 'values' from ( \
+            SELECT  WEEK(from_unixtime(epoch)) AS week_no, date(from_unixtime(epoch)) as week_start, \
+            line, avg(if (${measure} > 0, ${measure} , 0)) as ${measure} \
+            from power_data \
+            where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 YEAR)) \
+            GROUP BY WEEK(from_unixtime(epoch)), line \
+            ORDER BY week_no ) t group by epoch"
+      
+}
+
+const aggregatePower = (range_, byLine) => {
+  const line = byLine ? "group by line" : ""
+  const line_ = byLine ? "line, " : ""
+  const timePeriod = timePeriods[range_]
+  return fillTemplate(aggregatePowerQuery, {line_: line_, line: line, timePeriod: timePeriod})
+}
+
+const getValuesInRange = (range_, measure) => {
+  const values = measure === "voltage" ?  "voltage" : `group_concat(${measure} order by line)`
+  return fillTemplate(rangeQueries[range_], {values: values, measure: measure})
+}
+
+
 router.post("/insert", auth, (req, res, next) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
+  const data_ = req.body;
+  const data = data_.map((el) => Object.values(el));
+  con.query(insertQuery, [data], (err) => {
     if (err) {
-      res.sendStatus(403);
+      res.json({ status: "Error", data: err.stack });
     } else {
-      const data_ = req.body;
-      const data = data_.map((el) => Object.values(el));
-      const sql =
-        "INSERT INTO power_data (line, epoch, power, current ,voltage) VALUES ?";
-      con.query(sql, [data], (err) => {
-        if (err) {
-          res.json({ status: "Error", data: err.stack });
-        } else {
-          res.json({ status: "ok" });
-        }
-      });
+      res.json({ status: "ok" });
     }
   });
 });
 
 router.get("/lines", auth, (req, res, next) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
+  con.query(getLinesQuery, (err, result, fields) => {
     if (err) {
-      res.sendStatus(403);
+      res.status(400).json({ status: "Error", data: err.stack });
     } else {
-      con.query(
-        "select distinct line from power_data order by line",
-        (err, result, fields) => {
-          if (err) {
-            res.json({ status: "Error", data: err.stack });
-          } else {
-            res.json({ status: "ok", data: result });
-          }
-        }
-      );
+      res.status(200).json({ status: "ok", data: result });
     }
   });
 });
 
-router.post("/voltage", auth, (req, res, next) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
+router.post("/values", auth, (req, res, next) => {
+  const query_ = getValuesInRange(req.body.range, req.body.measure)
+  console.log(query_)
+  con.query(query_, (err, result, fields) => {
     if (err) {
-      res.sendStatus(403);
+      res.status(400).json({data: err.stack });
     } else {
-      const query_ = "select UNIX_TIMESTAMP(date_hour) * 1000 as epoch, voltage from (select DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), \
-      '%Y-%m-%d-%H') as date_hour, \
-      avg(voltage) as voltage from power_data \
-      where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -2 DAY)) * 1000 \
-      group by date_hour order by epoch ) t"
-      con.query(
-        query_,
-        (err, result, fields) => {
-          if (err) {
-            res.json({ status: "Error", data: err.stack });
-          } else {
-            res.json({ status: "ok", data: result });
-          }
-        }
-      );
+      res.status(200).json({data: result});
     }
-  });
-});
+  })
+})
+  
 
-router.post("/power", auth, (req, res, next) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      if (req.body.hasOwnProperty('aggregate')){
-        switch(req.body.aggregate){
-          case 'avg_day_line': var query_ = "select line, if(DAYOFWEEK(date_) > 6 or DAYOFWEEK(date_) = 1, 'weekend', 'weekday') as day, sum(kwh) as power_consumption \
-          from ( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, \
-                FROM_UNIXTIME(epoch/1000) as date_, line, \
-                avg(if(power > 0, power, 0))/1000 as kwh \
-            from power_data \
-            where epoch < UNIX_TIMESTAMP(timestamp(current_date)) * 1000 \
-            group by date_hour, line ) t \
-            group by line, day"
-          break
-          case 'total_month_line':  query_ = "select line, sum(kwh) as power_consumption from "  +
-                              "( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, " +
-                "FROM_UNIXTIME(epoch/1000) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
-            "where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 MONTH)) * 1000 \
-            group by date_hour, line ) t \
-          group by line" 
-          break
-          case 'total_three_months_line': query_ = "select line, sum(kwh) as power_consumption from "  +
-          "( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, " +
-          "FROM_UNIXTIME(epoch/1000) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
-          "where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -3 MONTH)) * 1000 \
-          group by date_hour, line ) t \
-          group by line" 
-          break 
-          case 'total_year_line':  query_ = "select line, sum(kwh) as power_consumption from "  +
-          "( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, " +
-          "FROM_UNIXTIME(epoch/1000) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
-          "where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 YEAR)) * 1000 \
-          group by date_hour, line ) t \
-          group by line" 
-          break
-          case 'avg_day':  query_ = "select if(DAYOFWEEK(date_) > 6 or DAYOFWEEK(date_) = 1, 'weekend', 'weekday') as day, sum(kwh) as power_consumption \
-          from ( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, \
-                FROM_UNIXTIME(epoch/1000) as date_, line, \
-                avg(if(power > 0, power, 0))/1000 as kwh \
-            from power_data \
-            where epoch < UNIX_TIMESTAMP(timestamp(current_date)) * 1000 \
-            group by date_hour, line ) t \
-            group by day"
-          break
-          case 'total_month':  query_ = "select sum(kwh) as power_consumption from "  +
-                              "( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, " +
-                "FROM_UNIXTIME(epoch/1000) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
-            "where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 MONTH)) * 1000 \
-            group by date_hour, line ) t" 
-          break
-          case 'total_three_months':  query_ = "select sum(kwh) as power_consumption from "  +
-          "( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, " +
-          "FROM_UNIXTIME(epoch/1000) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
-          "where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -3 MONTH)) * 1000 \
-          group by date_hour, line ) t" 
-          break 
-          case 'total_year':  query_ = "select sum(kwh) as power_consumption from "  +
-          "( select 	DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, " +
-          "FROM_UNIXTIME(epoch/1000) as date_, line, avg(if(power > 0, power, 0))/1000 as kwh from power_data " +
-          "where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -1 YEAR)) * 1000 \
-          group by date_hour, line ) t" 
-          break
-          default:  query_ = "select epoch, group_concat(power order by line) as 'values' \
-          from power_data group by epoch having count(*) > 1  order by epoch "
-        }
+const ranges = ["month", "three_months", "year"]
+const byLines = [true, false]
+
+router.post("/aggregate", auth, (req, res, next) => {
+  if (req.body.measure === "power"){
+    let params = []
+    ranges.forEach(range => byLines.forEach(byLine => params.push({range: range, byLine: byLine})))
+    console.log(params, params.length)
+
+    const queries = (params.map(p => aggregatePower(p.range, p.byLine))).join(";")
+    con.query(queries, (err, result, fields) => {
+      if (err) {
+        res.sendStatus(400)
+      } else {
+        res.status(200).json({summary: {
+          month_line: result[0],
+          month: result[1],
+          three_months_line: result[2],
+          three_months: result[3],
+          year_line: result[4],
+          year: result[5]
       }
-      else {
-        query_ = "select UNIX_TIMESTAMP(date_hour) * 1000 as epoch, group_concat(power order by line) \
-        as 'values' from (select DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, \
-        line, avg(if (power > 0, power, 0)) as power \
-        from power_data where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -2 DAY)) * 1000 \
-        GROUP by date_hour, line ) t group by epoch "
-      }
-      con.query(query_,
-        (err, result, fields) => {
-          if (err) {
-            res.json({ status: "Error", data: err.stack });
-          } else {
-            res.json({ status: "ok", data: result });
-          }
-        }
-      );
-    }
-  });
-});
-
-router.post("/current", auth, (req, res, next) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        "select UNIX_TIMESTAMP(date_hour) * 1000 as epoch, group_concat(current order by line) \
-        as 'values' from (select DATE_FORMAT((FROM_UNIXTIME(epoch/1000)), '%Y-%m-%d-%H') as date_hour, \
-        line, avg(if (current > 0, current, 0)) as current \
-        from power_data where epoch > UNIX_TIMESTAMP(DATE_ADD(now(), INTERVAL -2 DAY)) * 1000 \
-        GROUP by date_hour, line ) t group by epoch",
-        (err, result, fields) => {
-          if (err) {
-            res.json({ status: "Error", data: err.stack });
-          } else {
-            res.json({ status: "ok", data: result });
-          }
-        }
-      );
-    }
-  });
-});
+    })
+  }
+})
+}})
 
 module.exports = router;
