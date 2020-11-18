a483bf285d83190c3222262fb03120d7ec6849dd
cece95 <cesaregri@gmail.com>
2020-11-18 16:14:48 +0000

    Update lights.js

--- site/server/routes/api/lights.js
+++ site/server/routes/api/lights.js
@@ -11,363 +11,196 @@ const jwt = require("jsonwebtoken");
 let arr;
 var data;
 
-//gets all lights
-router.get("/:uid", auth, (req, res) =>
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        `SELECT 
-        lights.*, 
-        levels.id as levels_id,
-        levels.level,
-        buildings.building,
-        buildings.id as buildings_id,
-        sites.mqtt_topic_out,
-        sites.mqtt_topic_in,
-        sites.id as sites_id,
-        sites.name as sites_name
-      FROM
-        lights
-          LEFT OUTER JOIN
-        levels ON levels.id = lights.levels_id
-          LEFT OUTER JOIN
-        buildings ON buildings.id = levels.buildings_id
-          LEFT OUTER JOIN
-        sites ON sites.id = buildings.sites_id
-          LEFT OUTER JOIN
-        users_has_sites ON users_has_sites.sites_id = sites.id
-          LEFT OUTER JOIN
-        users ON users.id = users_has_sites.users_id
-      WHERE users.id = ? AND lights.is_assigned = 1`,
-        [req.params.uid],
-        (err, rows) => {
-          if (err) throw err;
-          if (rows.length) {
-            res.json(rows);
-          } else res.json([]);
-        }
-      );
-    }
+const getUserLights = `SELECT lg.*, l.id as levels_id, l.level, b.building, b.id as buildings_id,
+                      s.mqtt_topic_out, s.mqtt_topic_in,s.id as sites_id,
+                      s.name as sites_name
+                      FROM lights lg
+                      LEFT JOIN levels l ON l.id = lg.levels_id
+                      LEFT JOIN buildings b ON b.id = l.buildings_id
+                      LEFT JOIN sites s ON s.id = b.sites_id
+                      LEFT JOIN users_has_sites uhs ON uhs.sites_id = s.id
+                      LEFT JOIN users u ON u.id = uhs.users_id
+                      WHERE u.id = ? AND lg.is_assigned = ?`
+
+const getBuildingLights = `SELECT DISTINCT lg.id as lights_id, lg.node_id, lg.device_id, lg.type,
+                          lg.status, l.id as levels_id, l.level, b.building, b.id as buildings_id,
+                          s.mqtt_topic_out, s.mqtt_topic_in,s.id as sites_id,
+                          s.name as sites_name
+                          FROM lights lg
+                          LEFT JOIN levels l ON l.id = lg.levels_id
+                          LEFT JOIN buildings b ON b.id = l.buildings_id
+                          LEFT JOIN sites s ON s.id = b.sites_id
+                          LEFT JOIN users_has_sites uhs ON uhs.sites_id = s.id
+                          LEFT JOIN users u ON u.id = uhs.users_id
+                          WHERE b.id = ?`
+
+const getLevelLights = `SELECT DISTINCT lg.*, l.id as levels_id, l.level, b.building, 
+                        b.id as buildings_id, s.mqtt_topic_out, s.mqtt_topic_in, s.id as sites_id,
+                        s.name as sites_name
+                        FROM lights lg
+                        LEFT JOIN levels l ON l.id = lg.levels_id
+                        LEFT JOIN buildings b ON b.id = l.buildings_id
+                        LEFT JOIN sites s ON s.id = b.sites_id
+                        LEFT JOIN users_has_sites uhs ON uhs.sites_id = s.id
+                        LEFT JOIN users u ON u.id = uhs.users_id
+                        WHERE l.id = ? AND lg.is_assigned = 1`
+
+const getLastLightId = "SELECT id as last_id FROM lights ORDER BY id DESC LIMIT 1"
+const getLightByDeviceAndLevel = "SELECT id FROM lights WHERE device_id = ? AND levels_id = ?"
+const insertLight = "INSERT INTO lights SET ?"
+const createEmptyLights = "INSERT INTO lights (level_id) VALUES ?"
+const updateLightPosition = `UPDATE lights SET fp_coordinates_left = ?, fp_coordinates_bot = ? WHERE id = ?`
+const updateLight = `UPDATE lights SET ? WHERE id = ?`
+const assignLight = "UPDATE lights SET levels_id = ?, is_assigned=1 WHERE id=?"
+const moveLight = "UPDATE lights SET levels_id=? WHERE id=?"
+const deleteLight = "DELETE FROM lights WHERE id = ?"
+
+
+router.get("/:uid", auth, (req, res) => {
+  con.query(getUserLights, [req.params.uid, 1], (err, rows) => {
+    if (err) throw err
+    res.json(rows)
   })
-),
-  router.get("/unassigned/:uid", auth, (req, res) =>
-    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        con.query(
-          `SELECT 
-        lights.*, 
-        levels.id as levels_id,
-        levels.level,
-        buildings.building,
-        buildings.id as buildings_id,
-        sites.mqtt_topic_out,
-        sites.mqtt_topic_in,
-        sites.id as sites_id,
-        sites.name as sites_name
-      FROM
-        lights
-          LEFT OUTER JOIN
-        levels ON levels.id = lights.levels_id
-          LEFT OUTER JOIN
-        buildings ON buildings.id = levels.buildings_id
-          LEFT OUTER JOIN
-        sites ON sites.id = buildings.sites_id
-          LEFT OUTER JOIN
-        users_has_sites ON users_has_sites.sites_id = sites.id
-          LEFT OUTER JOIN
-        users ON users.id = users_has_sites.users_id
-      WHERE users.id = ? AND lights.is_assigned = 0`,
-          [req.params.uid],
-          (err, rows) => {
-            if (err) throw err;
-            res.json(rows);
-          }
-        );
-      }
-    })
-  ),
-  //gets all lights
-  router.get("/building/:buildings_id", auth, (req, res) =>
-    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        con.query(
-          `SELECT 
-          lights.id as lights_id,
-          lights.node_id,
-          lights.device_id,
-          lights.type,
-          lights.status,
-          levels.id as levels_id,
-          levels.level,
-          buildings.building,
-          buildings.id as buildings_id,
-          sites.mqtt_topic_out,
-          sites.mqtt_topic_in,
-          sites.id as sites_id,
-          sites.name as sites_name
-        FROM
-          lights
-            LEFT OUTER JOIN
-          levels ON levels.id = lights.levels_id
-            LEFT OUTER JOIN
-          buildings ON buildings.id = levels.buildings_id
-            LEFT OUTER JOIN
-          sites ON sites.id = buildings.sites_id
-            LEFT OUTER JOIN
-          users_has_sites ON users_has_sites.sites_id = sites.id
-            LEFT OUTER JOIN
-          users ON users.id = users_has_sites.users_id
-        WHERE buildings.id = ?
-        GROUP BY lights_id`,
-          [req.params.buildings_id],
-          (err, rows) => {
-            if (err) throw err;
-            if (rows.length) {
-              res.json(rows);
-            } else
-              con.query(
-                "SELECT id as last_id FROM lights ORDER BY id DESC LIMIT 1",
-                (err, rows) => res.json(rows)
-              );
-          }
-        );
-      }
-    })
-  ),
-  router.get("/level/:level_id", auth, (req, res) =>
-    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        con.query(
-          `SELECT 
-        lights.*, 
-        levels.id as levels_id,
-        levels.level,
-        buildings.building,
-        buildings.id as buildings_id,
-        sites.mqtt_topic_out,
-        sites.mqtt_topic_in,
-        sites.id as sites_id,
-        sites.name as sites_name
-      FROM
-        lights
-          LEFT OUTER JOIN
-        levels ON levels.id = lights.levels_id
-          LEFT OUTER JOIN
-        buildings ON buildings.id = levels.buildings_id
-          LEFT OUTER JOIN
-        sites ON sites.id = buildings.sites_id
-          LEFT OUTER JOIN
-        users_has_sites ON users_has_sites.sites_id = sites.id
-          LEFT OUTER JOIN
-        users ON users.id = users_has_sites.users_id
-      WHERE levels.id = ? AND lights.is_assigned = 1
-	GROUP BY lights.id, levels.id`,
-          [req.params.level_id],
-          (err, rows) => {
-            if (err) throw err;
-            if (rows.length) {
-              res.json(rows);
-            } else res.json([]);
-          }
-        );
-      }
-    })
-  ),
-  router.get("/device_id/:device_id/:levels_id", auth, (req, res) =>
-    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        con.query(
-          "SELECT id FROM lights WHERE device_id = ? AND levels_id = ?",
-          [req.params.device_id, req.params.levels_id],
-          (err, rows) => res.json(rows)
-        );
-      }
-    })
-  ),
-  router.post("/add", auth, function (req, res) {
-    jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-      if (err) {
-        res.sendStatus(403);
-      } else {
-        con.query(
-          "INSERT INTO lights SET ?",
-          [
-            {
-              device_id: req.body.device_id,
-              node_id: req.body.node_id,
-              type: req.body.type,
-              levels_id: req.body.levels_id,
-            },
-          ],
-          (err, results) => {
-            if (err) res.send(err).status(400);
-            res.sendStatus(200);
-          }
-        );
-      }
-    });
-  });
+})
+
+
+router.get("/unassigned/:uid", auth, (req, res) => {
+  con.query(getUserLights, [req.params.uid, 0], (err, rows) => {
+    if (err) throw err
+    res.json(rows)
+  })
+})
+
+router.get("/building/:buildings_id", auth, (req, res) => {
+  con.query(getBuildingLights, req.params.buildings_id, (err, rows) => {
+    if (err) throw err
+    if (rows.length) {
+      res.json(rows);
+    } 
+    else con.query(getLastLightId, (err, rows) => res.json(rows)) 
+  })
+})
+
+router.get("/level/:level_id", auth, (req, res) => {
+  con.query(getLevelLights, req.params.level_id, (err, rows) => {
+    if (err) throw err
+    res.json(rows)
+  })
+})
+
+router.get("/device_id/:device_id/:levels_id", auth, (req, res) => {
+  const params = [req.params.device_id, req.params.levels_id]
+  con.query(getLightByDeviceAndLevel, params, (err, rows) => {
+    if (err) throw err 
+    res.json(rows)
+  })
+})
+
+router.post("/add", auth, function (req, res) {
+  const params = {
+    device_id: req.body.device_id,
+    node_id: req.body.node_id,
+    type: req.body.type,
+    levels_id: req.body.levels_id,
+  }
+  con.query(insertLight, [params], (err) => {
+    if (err) throw err 
+    res.sendStatus(200)
+  })
+})
+
 router.post("/addempty/:amount", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      console.log("Add", req.params.amount, "empty devices");
-
-      res.sendStatus(200);
-    }
-  });
-});
-
-router.post("/edit/many", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      console.log("edit");
-      req.body.devices.forEach((el) => {
-        con.query(
-          `UPDATE emergencyegg.lights
-        SET fp_coordinates_left = ?,
-        fp_coordinates_bot = ?
-        WHERE id = ?
-        `,
-          [el.fp_coordinates_left, el.fp_coordinates_bot, el.id],
-          (err, results) => {
-            if (err) throw err;
-          }
-        );
-      });
-      res.sendStatus(200);
-    }
-  });
-});
-
-//bulk edit
-router.post("/bulkedit", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      const updateData = Object.keys(req.body.update_data).map(
-        (i) => req.body.update_data[i]
-      );
-      updateData.forEach((el) => {
-        con.query(`UPDATE lights SET ? WHERE id = ?`, [
-          {
-            device_id: el.newData.device_id,
-            type: el.newData.type,
-            node_id: el.newData.node_id,
-          },
-          el.newData.id,
-        ]);
-      });
-      res.sendStatus(200);
-    }
-  });
-});
-
-router.post("/edit/single/:id", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        "UPDATE lights SET ? WHERE id=?",
-        [
-          {
-            device_id: req.body.device_id,
-            node_id: req.body.node_id,
-            type: req.body.type,
-            levels_id: req.body.levels_id,
-          },
-          req.params.id,
-        ],
-        function (error, results, fields) {
-          if (error) throw error;
-          res.end(JSON.stringify(results));
-        }
-      );
-    }
-  });
-});
-
-router.post("/edit/:id", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        "UPDATE lights SET levels_id = ?, is_assigned=1 WHERE id=?",
-        [req.body.levels_id, req.params.id],
-        function (error, results, fields) {
-          if (error) throw error;
-          res.end(JSON.stringify(results));
-        }
-      );
-    }
-  });
-});
-
-router.post("/move/:id", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        "UPDATE lights SET levels_id=? WHERE id=?",
-        [req.body.levels_id, req.params.id],
-        function (error, results, fields) {
-          if (error) throw error;
-          res.end(JSON.stringify(results));
-        }
-      );
-    }
-  });
-});
-
-// Delete chosen light
-router.delete("/:id", auth, function (req, res) {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        "DELETE FROM lights WHERE id = ?",
-        [req.params.id],
-        (err, results) => {
-          if (err) throw err;
-          res.sendStatus(200);
-        }
-      );
-    }
-  });
-});
-
-router.get("/lastid", auth, (req, res) =>
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(
-        "SELECT id as last_id FROM lights ORDER BY id DESC LIMIT 1",
-        (err, rows) => {
-          if (err) throw err;
-          console.log(rows);
-          res.json(rows[0]);
-        }
-      );
-    }
+  const amount = req.params.amount
+  const levelId = req.body.level_id 
+  let params = []
+  for (let i = 0; i < amount; i++) {
+    params.push([levelId])
+  }
+  con.query(createEmptyLights, [params], (err) => {
+    if (err) throw err 
+    res.sendStatus(200)
   })
-),
-  (module.exports = router);
+})
+
+router.post("/edit/postion", auth, function (req, res) {
+  const paramsList = req.body.devices.map(el => [el.fp_coordinates_left, el.fp_coordinates_bot, el.id])
+  
+  for (let i = 0; i < devices.length; i++) {
+    con.query(updateLightPosition, paramsList[i], (err) => {
+      if (err) throw err 
+      if (i === paramsList.length - 1){
+        res.sendStatus(200)
+      }
+    })
+  }
+})
+
+router.post("/edit/many", auth, (req, res) => {
+  const paramsList = Object.keys(req.body.update_data)
+    .map(i => req.body.update_data[i])
+    .map(el => [
+        {
+          device_id: el.newData.device_id,
+          type: el.newData.type,
+          node_id: el.newData.node_id,
+        },
+        el.newData.id,
+      ])
+
+    for (let i = 0; i < paramsList.length; i++) {
+      con.query(updateLight, paramsList[i], (err) => {
+        if (err) throw err 
+        if (i === paramsList.length - 1){
+          res.sendStatus(200)
+        }
+      })    
+    }
+})
+
+router.post("/edit/single/:id", auth, (req, res) => {
+  const params = [
+    {
+      device_id: req.body.device_id,
+      node_id: req.body.node_id,
+      type: req.body.type,
+      levels_id: req.body.levels_id,
+    },
+    req.params.id,
+  ]
+  con.query(updateLight, params, (err) => {
+    if (err) throw err 
+    res.sendStatus(200)
+  })
+})
+
+router.post("/assign/:id", auth, (req, res) => {
+  const params = [req.body.levels_id, req.params.id]
+  con.query(assignLight, params, (err) => {
+    if (err) throw err 
+    res.sendStatus(200)
+  })
+})
+
+router.post("/move/:id", auth, (req, res) => {
+  const params = [req.body.levels_id, req.params.id]
+  con.query(moveLight, params, (err) => {
+    if (err) throw err 
+    res.sendStatus(200)
+  })
+})
+
+router.delete("/:id", auth, (req, res) => {
+  con.query(deleteLight, req.params.id, (err) => {
+    if (err) throw err 
+    res.sendStatus(200)
+  })
+})
+
+router.get("/lastid", auth, (req, res) => {
+  con.query(getLastLightId, (err, rows) => {
+    console.log(rows)
+    res.json(rows[0])
+  })
+})
+
+module.exports = router
