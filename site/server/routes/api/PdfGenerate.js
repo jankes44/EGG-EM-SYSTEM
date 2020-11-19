cc38253d107c90a95333b81b43eaed48abe1f35a
cece95 <cesaregri@gmail.com>
2020-11-19 08:46:21 +0000

    removed duplicate code in pdfGenerate

--- site/server/routes/api/PdfGenerate.js
+++ site/server/routes/api/PdfGenerate.js
@@ -10,6 +10,27 @@ let ejs = require("ejs");
 let pdf = require("html-pdf");
 let path = require("path");
 
+const pdfPaths = {
+  "official": path.join(__dirname, "../../pdfTemplates/PdfTemplateOfficial.ejs"),
+  "simple": path.join(__dirname, "../../pdfTemplates/PdfTemplateSimple.ejs")
+}
+
+const pdfOptions = { 
+  height: "11.25in",
+  width: "8.5in",
+  header: {
+    height: "20mm",
+  },
+  footer: {
+    height: "20mm",
+  },
+}
+
+const pdfResponseHeader = {
+  "Content-Type": "application/force-download",
+  "Content-disposition": "attachment; filename=file.pdf",
+}
+
 const reportDataQuery = `SELECT 
                     trial_tests_has_lights.trial_tests_id AS test_id,
                     trial_tests_has_lights.result AS device_response,
@@ -50,128 +71,50 @@ const sortCompare = (a, b) => {
 
 const generateReport = (rows, res, reportType = "official") => {
   let data_ = rows;
+  let faults = {"OK": 0, "Lamp Fault": 0, "Battery Fault": 0, "Weak connection to mesh": 0}
   let lampFault = 0;
   let battFault = 0;
   let noConnection = 0;
   let noFault = 0;
   console.log(data_);
   data_.forEach((el) => {
-    switch (el.device_response) {
-      case "OK":
-        noFault++;
-        break;
-      case "Lamp Fault":
-        lampFault++;
-        break;
-      case "Battery Fault":
-        battFault++;
-        break;
-      case "Weak connection to mesh":
-        noConnection++;
-        break;
-    }
+    const response = el.device_response
+    faults[response] = faults[response] + 1
   });
-  let responsesData = {
-    lampFault: lampFault,
-    battFault: battFault,
-    noConnection: noConnection,
-    noFault: noFault,
+  const responsesData = {
+    lampFault: faults["Lamp Fault"],
+    battFault: faults["Battery Fault"],
+    noConnection: faults["Weak connection to mesh"],
+    noFault: faults["OK"],
   };
 
   data_.sort(sortCompare);
+  const pdfData = { data: data_, resData: responsesData }
 
-  switch (reportType) {
-    case "official":
-      ejs.renderFile(
-        path.join(__dirname, "../../pdfTemplates/PdfTemplateOfficial.ejs"),
-        { data: data_, resData: responsesData },
-        (err, data) => {
-          if (err) {
-            res.send(err);
-          } else {
-            let options = {
-              height: "11.25in",
-              width: "8.5in",
-              header: {
-                height: "20mm",
-              },
-              footer: {
-                height: "20mm",
-              },
-            };
-            pdf.create(data, options).toStream(function (err, stream) {
-              if (err) {
-                res.send(err);
-              } else {
-                res.writeHead(200, {
-                  "Content-Type": "application/force-download",
-                  "Content-disposition": "attachment; filename=file.pdf",
-                });
-                stream.pipe(res);
-              }
-            });
-          }
-        }
-      );
-      break;
-    case "simple":
-      ejs.renderFile(
-        path.join(__dirname, "../../pdfTemplates/PdfTemplateSimple.ejs"),
-        { data: data_, resData: responsesData },
-        (err, data) => {
-          if (err) {
-            res.send(err);
-          } else {
-            let options = {
-              height: "11.25in",
-              width: "8.5in",
-              header: {
-                height: "20mm",
-              },
-              footer: {
-                height: "20mm",
-              },
-            };
-            pdf.create(data, options).toStream(function (err, stream) {
-              if (err) {
-                return err;
-              } else {
-                res.writeHead(200, {
-                  "Content-Type": "application/force-download",
-                  "Content-disposition": "attachment; filename=file.pdf",
-                });
-                stream.pipe(res);
-              }
-            });
-          }
-        }
-      );
-      break;
-  }
-};
+  ejs.renderFile(pdfPaths[reportType], pdfData, (err, data) => {
+    if (err) res.sendStatus(400)
+    pdf.create(data, options).toStream((err, stream) => {
+      if (err) res.sendStatus(400)
+        res.writeHead(200, pdfResponseHeader)
+        stream.pipe(res)
+    
+    })
+  })
+}
 
 router.get("/generateReport/:test_id", auth, (req, res) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(reportDataQuery, req.params.test_id, (err, rows) => {
-        generateReport(rows, res, "simple");
-      });
-    }
+    con.query(reportDataQuery, req.params.test_id, (err, rows) => {
+      if (err) res.sendStatus(400)
+      generateReport(rows, res, "simple");
   });
-});
+})
 
 router.get("/generateOfficialReport/:test_id", auth, (req, res) => {
-  jwt.verify(req.token, process.env.SECRET_KEY, (err) => {
-    if (err) {
-      res.sendStatus(403);
-    } else {
-      con.query(reportDataQuery, [req.params.test_id], (err, rows) => {
-        generateReport(rows, res, "official");
-      });
-    }
-  });
-});
+    con.query(reportDataQuery, [req.params.test_id], (err, rows) => {
+      if (err) res.sendStatus(400)
+      generateReport(rows, res, "official");
+    })
+})
 
-module.exports = router;
+
+module.exports = router
