1c4286a6ab45b96ed3f88614ed7661218c562391
Jarek Cymbalista <kalima12345@gmail.com>
2020-12-01 13:40:09 +0000

    Update UploadCSV.js

--- site/client/src/components/UploadCSV/UploadCSV.js
+++ site/client/src/components/UploadCSV/UploadCSV.js
@@ -1,5 +1,5 @@
-import React, { useState } from "react";
-import { Button } from "@material-ui/core";
+import React, {useState} from "react";
+import {Button} from "@material-ui/core";
 import MaterialTable from "material-table";
 import CSVReader from "react-csv-reader";
 
@@ -7,8 +7,17 @@ export default function UploadCSV(props) {
   const [rendered, setRendered] = useState(false);
   const [columns, setColumns] = useState([]);
   const [data, setData] = useState([]);
+  const [deviceCols] = useState([
+    "id",
+    "node_id",
+    "type",
+    "levels_id",
+    "buildings_id",
+    "level",
+    "building",
+  ]);
 
-  function csvJSON(csv) {
+  const csvJSON = (csv) => {
     console.log(csv);
     var result = [];
 
@@ -27,25 +36,59 @@ export default function UploadCSV(props) {
 
     //return result; //JavaScript object
     return result; //JSON
-  }
+  };
+
+  const onFileLoaded = (data, fileInfo) => {
+    let c = [];
+    data[0].map((el) => {
+      c.push({field: el, title: el});
+    });
+
+    const dataJSON = csvJSON(data);
+    const dataLimited = dataJSON.slice(0, 5);
+
+    console.log(autoMapping(data[0]));
+    // console.log(prepareCsvForInsert(dataJSON));
+
+    setColumns(c);
+    setData(dataLimited);
+    setRendered(true);
+  };
+
+  const autoMapping = (cols) => {
+    let mapping = [];
+    const re = new RegExp(/\s|-|_/g);
+    cols.forEach((c) => {
+      let c_ = c.toLowerCase().replace(re, "");
+      deviceCols.forEach((dc) => {
+        let dc_ = dc.toLowerCase().replace(re, "");
+        console.log(dc_, c_);
+        if (dc_ === c_) mapping.push({[dc]: c});
+      });
+    });
+    return mapping;
+  };
+
+  const prepareCsvForInsert = (input) => {
+    const mapping = {node: "node_id", id: "id"};
+
+    return input.map((n) =>
+      Object.keys(n)
+        .filter((key) => mapping.hasOwnProperty(key))
+        .reduce((obj, key) => {
+          obj[mapping[key]] = n[key];
+          return obj;
+        }, {})
+    );
+  };
 
   return (
     <div>
       {rendered && columns.length > 0 ? (
         <MaterialTable columns={columns} data={data} title="Data" />
       ) : null}
-      <CSVReader
-        onFileLoaded={(data, fileInfo) => {
-          let c = [];
-          data[0].map((el) => {
-            c.push({ field: el, title: el });
-          });
-
-          setColumns(c);
-          setData(csvJSON(data));
-          setRendered(true);
-        }}
-      />
+      <CSVReader onFileLoaded={onFileLoaded} />
+      <ul></ul>
     </div>
   );
 }
